df2ad34542c40bec0c1895015c4ab33fc424615f
Samir Jabre <73381924>
2024-09-12 15:17:37 +0300

    Created the structure of the component

---
+++ Components/MainButtons.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainButtons = () => {
+  return (
+    <View>
+      <Text>MainButtons</Text>
+    </View>
+  )
+}
+
+export default MainButtons
+
+const styles = StyleSheet.create({})
