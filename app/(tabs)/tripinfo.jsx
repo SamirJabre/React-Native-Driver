dc4841513eda077c06379ae9afee129cdabd25a0
Samir Jabre <73381924>
2024-09-12 18:55:57 +0300

    building the structure of the page

---
+++ app/(tabs)/tripinfo.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const tripinfo = () => {
+  return (
+    <View>
+      <Text>tripinfo</Text>
+    </View>
+  )
+}
+
+export default tripinfo
+
+const styles = StyleSheet.create({})
