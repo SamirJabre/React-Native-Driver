5356745593db6446377c562fd7193245d702af12
Samir Jabre <73381924>
2024-09-13 15:57:52 +0300

    Adding all states to use

--- app/(auth)/form.jsx
+++ app/(auth)/form.jsx
@@ -5,3 +5,19 @@ import axios from 'axios';
 import { BASE_URL } from '@env';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../firebaseConfig';
+
+
+export const form = () ={
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [age, setAge] = useState('');
+  const [address, setAddress] = useState('');
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [idPhoto, setIdPhoto] = useState(null);
+  const [driverLicense, setDriverLicense] = useState(null);
+  const [downloadUrl, setDownloadUrl] = useState(null);
+
+}
