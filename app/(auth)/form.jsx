3b8b87a6766147f6bd6ca679dc44d344db4b3921
Samir Jabre <73381924>
2024-09-13 15:58:40 +0300

    Created the Image picker function

--- app/(auth)/form.jsx
+++ app/(auth)/form.jsx
@@ -30,4 +30,18 @@ export const form = () ={
     })();
   }, []);
 
+
+  const pickImage = async (setState) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setState(result.assets[0].uri);
+    }
+  };
+
 }
